8d758ca217a7344fc487c4075d589d7e241c2631
Dariusz Zielonka <dariuszzielonka727@gmail.com>
2022-11-07 13:58:15 +0100

    irrelevant commit

--- src/main/java/com/zse4p/movieapi/DataLoader.java
+++ src/main/java/com/zse4p/movieapi/DataLoader.java
@@ -1,5 +1,7 @@
 package com.zse4p.movieapi;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -7,7 +9,7 @@ import org.springframework.stereotype.Component;
 import com.zse4p.movieapi.models.Movie;
 import com.zse4p.movieapi.models.User;
 import com.zse4p.movieapi.repositories.MovieRepo;
-import com.zse4p.movieapi.repositories.UserRepo;
+import com.zse4p.movieapi.services.UserService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -17,17 +19,17 @@ public class DataLoader implements CommandLineRunner {
 	private MovieRepo movieRepo;
 
 	@Autowired
-	private UserRepo userRepo;
+	private UserService userService;
 
     @Override
     public void run(String... args) throws Exception {
         
-        movieRepo.save(new Movie("Gayniggers From Outer Space", 1992, "Robert Lewandowski"));
-        movieRepo.save(new Movie("Marta mowi", 2008, "Jan Pawel Drugi"));
+        movieRepo.save(new Movie("The Truman Show", 1998, "Robert Lewandowski", List.of(Genre.Horror)));
+        movieRepo.save(new Movie("Marta mowi", 2008, "Jan Pawel Drugi", List.of(Genre.Animated)));
     
-        userRepo.save(new User("user", "pass", UserRole.User));
-        userRepo.save(new User("critic", "pass", UserRole.Critic));
-        userRepo.save(new User("moderator", "pass", UserRole.Moderator));
+        userService.registerUser(new User("user", "pass", UserRole.User));
+        userService.registerUser(new User("critic", "pass", UserRole.Critic));
+        userService.registerUser(new User("moderator", "pass", UserRole.Moderator));
     
     }
     
